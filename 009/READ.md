4001044e87345becd26405760429e746bd6a196a
svrLinux <sv_voronin@inbox.ru>
2015-10-06 08:07:33 -0700

    on Lesson

---
+++ 009/READ.md
@@ -0,0 +1,3 @@
+https://www.opennet.ru/docs/RUS/linux_parallel/node16.html
+https://www.opennet.ru/docs/RUS/linux_parallel/node16.html
+http://citforum.ru/programming/unix/ipc_intro/
---
+++ 009/main.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h> 
+#include <string.h>
+
+#define MAX 1024
+
+int main (int arg, char *argv[] )
+{
+
+FILE *pip_w;
+FILE *fl;
+char buff [MAX];
+char buff_all [MAX];
+int i = 0;
+int n = 0;
+
+pip_w= popen (argv[1], "r");
+if (pip_w == NULL) { printf ("exit fl == NULL\n"); return 0;}
+
+while ( fgets (buff, MAX, pip_w ) ) { 
+n= strlen (buff);
+
+for (i=0; i<n; i++) {
+  char p;
+  p = buff [i];
+  buff_all [i]
+  printf ("%s\n", buff);
+}
+
+
+printf ("%s", buff);
+
+}
+
+printf("\n");
+
+pclose(pip_w);
+
+return 0;
+}
